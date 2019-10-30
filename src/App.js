660393f2dd8ae486cc4751cfb525d690af8bee9a
Kritsatorn <kritsatorn@billme.co.th>
2019-10-30 15:54:59 +0700

    updated: added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        {/* <img src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/logo/banner.png"/> */}
         FUCK
       </div>
     );
